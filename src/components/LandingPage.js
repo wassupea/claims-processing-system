c21f2d523b71e045641be607be6d31878ef1ec63
wassupea <15amikadelavin@gmail.com>
2023-08-14 14:33:10 +0800

    test

---
+++ src/components/LandingPage.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import '../componentsCSS/Landingpage.scss';
+
+function LandingPage() {
+
+
+  return (
+    <>
+      <div>
+        <body>
+            <div className='img-container'>
+            <img src="/images/holdinghands.png" className='image_body' alt="image" />
+            </div>
+            <div>
+          
+            </div>
+        </body>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage;
